0b5896fa4730ec2ed802426889769ff16dff5436
Alexandre <alexandre_bernardi95@hotmail.com>
2025-02-25 20:41:48 -0300

    start spring project

---
+++ src/test/java/com/app/bibliotecaAPI/BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bibliotecaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
